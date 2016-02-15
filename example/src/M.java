751a8d0407307bb3813d6a9c259be380e2724c2c
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-02-15 16:36:26 -0500

    add an running example

---
+++ example/src/M.java
@@ -0,0 +1,47 @@
+class M {
+	public static void main(String [] argv){
+		M obj = new M();
+		if (argv.length > 0)
+			obj.m(argv[0], argv.length);
+	}
+	
+	public void m(String arg, int i) {
+		int q = 1;
+		A o = new A();
+		Impossible nothing = new Impossible();
+		if (i == 0)
+			q = 4;
+		q++;
+		switch (arg.length()) {
+			case 0: q /= 2; break;
+			case 1: o = new A(); new B(); q = 25; break;
+			case 2: o = new A(); q = q * 100;
+			default: o = new B(); break; 
+		}
+		if (arg.length() > 0) {
+			o.f();
+		} else {
+			System.out.println("zero");
+		}
+		nothing.happened();
+	}
+}
+
+class A {
+	public void f() { 
+		System.out.println("a");
+	}
+}
+
+class B extends A {
+	public void f() { 
+		System.out.println("b");
+	}
+}
+
+class Impossible{
+	public void happened() {
+		// "2b||!2b?", whatever the answer nothing happens here
+	}
+}
+
