c0d1edcdc118f1549ac71bce42e9c5f6af8397b7
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-02-15 12:14:45 -0500

    init new file structure

---
+++ README.md
@@ -0,0 +1,14 @@
+# CFGVisualization
+Control Flow Graph Visualization for Checker Framework
+
+#Requirement
+
+This project depends on following four projects:
+
+https://bitbucket.org/typetools/jsr308-langtools
+
+https://github.com/typetools/annotation-tools
+
+https://github.com/typetools/checker-framework
+
+https://github.com/typetools/checker-framework-inference 
---
+++ src/DOTCFGVisualizer/DOTCFGVisualizerOnlyType.java
@@ -0,0 +1,57 @@
+package DOTCFGVisualizer;
+
+import org.checkerframework.dataflow.cfg.DOTCFGVisualizer;
+import org.checkerframework.dataflow.analysis.AbstractValue;
+import org.checkerframework.dataflow.analysis.Analysis;
+import org.checkerframework.dataflow.analysis.Store;
+import org.checkerframework.dataflow.analysis.TransferFunction;
+import org.checkerframework.dataflow.analysis.FlowExpressions;
+import org.checkerframework.dataflow.cfg.node.Node;
+
+
+public class DOTCFGVisualizerOnlyType <A extends AbstractValue<A>,
+S extends Store<S>, T extends TransferFunction<A, S>> extends DOTCFGVisualizer<A, S, T> {
+	
+	@Override
+	protected String visualizeBlockNode(Node t, /*@Nullable*/ Analysis<A, S, T> analysis) {
+        return prepareString(t.toString()) + "   [ " + visualizeType(t) + " ]" + t.getType();
+    }
+	
+	@Override
+	public void visualizeLocalVariable(FlowExpressions.LocalVariable key, A value) {
+		this.sbStore.append("  " + key + " > " +
+				key.getType()
+                + "\\n");
+		
+	}
+
+	@Override
+	public void visualizeFieldValues(FlowExpressions.FieldAccess fieldAccess, A value) {
+		this.sbStore.append("  " + fieldAccess + " > " +
+				fieldAccess.getType()
+                + "\\n");
+	}
+
+	@Override
+	public void visualizeArrayAccess(FlowExpressions.ArrayAccess arrayAccess, A value) {
+		this.sbStore.append("  " + arrayAccess + " > " +
+				arrayAccess.getType()
+                + "\\n");
+		
+	}
+
+	@Override
+	public void visualizeMethodValues(FlowExpressions.PureMethodCall methodCall, A value) {
+		this.sbStore.append("  " + methodCall.toString().replace("\"", "\\\"")
+                + " > " + methodCall.getType() + "\\n");
+		
+	}
+
+	@Override
+	public void visualizeClassValues(FlowExpressions.ClassName className, A value) {
+		this.sbStore.append("  " + className + " > " + className.getType()
+                + "\\n");
+		
+	}
+
+}
